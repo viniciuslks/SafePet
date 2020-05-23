13b3c5e5da3c5612104fda4fbb6f3fd1c459353c
GuilhermeBergamoBR <bergamoguilherme14@gmail.com>
2020-05-23 13:20:05 -0300

    o nodemon ajudara na inicializacao do server

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3001, function () {
+  console.log('Servidor rodando na porta 3001!');
 });
+
+//PARA STARTAR O SERVIDOR RODAR NO TERMINAL/PROMPT DE COMANDO: npm run dev(estando dentro do diretório "backend")
